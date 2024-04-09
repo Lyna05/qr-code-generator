1095c2b01a34d41d97494b421d2794494bf0f3f2
Lyna05 <lyne_armelle@hotmail.com>
2024-04-09 14:41:21 +0200

    Adding sync/async examples and ha.

---
+++ index.js
@@ -0,0 +1,33 @@
+import inquirer from 'inquirer';
+import qr from 'qr-image';
+import fs from 'fs';
+inquirer
+  .prompt([
+    /* Pass your questions in here */
+    {
+        message: "Gebe eine URL hier ein: ",
+        name: "URL"
+    }
+  ])
+  .then((answers) => {
+    // Use user feedback for... whatever!!
+    const url = answers.URL
+    console.log(url)
+    
+    var qr_png = qr.image(url);
+    qr_png.pipe(fs.createWriteStream('qr_code.png'));
+    
+    fs.writeFile('url.txt', url, (err) => {
+        if (err) throw err;
+        console.log('The file has been saved!');
+
+  });
+  })
+
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
